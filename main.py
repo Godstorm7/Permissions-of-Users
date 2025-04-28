1cc012ed81f96f27ce717037c19f16be6b9379b0
SamuelD <samueldantas1227@gmail.com>
2025-04-28 19:41:54 -0300

    criei mais alguns usuarios

--- main.py
+++ main.py
@@ -2,16 +2,21 @@
 from Model.Usuario import Usuario
 from Model.Arquivo import Arquivo
 
-usuario1 = Usuario("Alice")
-usuario2 = Usuario("Bob")
-usuario3 = Usuario("Samuel")
+usuario1 = Usuario("Brenda")
+usuario2 = Usuario("Junior")
+usuario3 = Usuario("Yan")
+usuario4 = Usuario("Samuel")
 
 # Criando arquivo
 arquivo = Arquivo("documento.txt", usuario1)
 
 # Definindo permissões
 arquivo.definir_permissao(usuario1, leitura=True, escrita=True, execucao=False)
+
 arquivo.definir_permissao(usuario2, leitura=True, escrita=False, execucao=False)
+
+arquivo.definir_permissao(usuario3, leitura=True)
+
 
 
 
@@ -32,3 +37,9 @@ print("----------------------------------------------------------")
 print(arquivo.verificar_permissao(usuario3, "leitura"))
 print(arquivo.verificar_permissao(usuario3, "escrita"))
 print(arquivo.verificar_permissao(usuario3, "execucao"))
+
+print("----------------------------------------------------------")
+
+print(arquivo.verificar_permissao(usuario4, "leitura"))
+print(arquivo.verificar_permissao(usuario4, "escrita"))
+print(arquivo.verificar_permissao(usuario4, "execucao"))
