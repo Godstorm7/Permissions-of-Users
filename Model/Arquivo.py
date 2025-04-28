519db88cd020fe053720f85efd806de899a2338e
SamuelD <samueldantas1227@gmail.com>
2025-04-28 19:35:40 -0300

    pequenas alterações

--- Model/Arquivo.py
+++ Model/Arquivo.py
@@ -1,17 +1,20 @@
 class Arquivo:
-    def __init__(self, nome, dono):
-        self.nome = nome
-        self.dono = dono
-        self.permissoes = {}
+            def __init__(self, nome, dono):
+                self.nome = nome
+                self.dono = dono
+                self.permissoes = {}
 
-    def definir_permissao(self, usuario, leitura=False, escrita=False, execucao=False):
-        self.permissoes[usuario.nome] = {
-            'leitura': leitura,
-            'escrita': escrita,
-            'execucao': execucao
-        }
+            def definir_permissao(self, usuario, leitura=False, escrita=False, execucao=False):
+                self.permissoes[usuario.nome] = {
+                    'leitura': leitura,
+                    'escrita': escrita,
+                    'execucao': execucao
+                }
 
-    def verificar_permissao(self, usuario, tipo):
-        # Corrigido para evitar erro caso o usuário não tenha permissões definidas
-        permissoes_usuario = self.permissoes.get(usuario.nome, {})
-        return f"Permissão de {tipo} para o usuário '{usuario.nome}': {permissoes_usuario.get(tipo, False)}"
+            def verificar_permissao(self, usuario, tipo):
+                # Corrigido para evitar erro caso o usuário não tenha permissões definidas
+                permissoes_usuario = self.permissoes.get(usuario.nome, {})
+                for permissao, valor in permissoes_usuario.items():
+                    if permissao == tipo:
+                        return f"Permissão de {tipo} para o usuário '{usuario.nome}': {valor}"
+                return f"Permissão de {tipo} para o usuário '{usuario.nome}': False"
